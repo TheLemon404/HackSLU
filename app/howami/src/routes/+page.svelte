c5ed57279c1e26a56be24f8697bb14e28cd5a097
Michael Teschner <120539187+BlazeTechDev@users.noreply.github.com>
2025-03-22 12:19:58 -0500

    fonts in css

--- app/howami/src/routes/+page.svelte
+++ app/howami/src/routes/+page.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <div>
-    <div class="convorsation_container">
-        <input>
+    <div class="conversation_container">
+        <input placeholder="whats on your mind?">
     </div>
 
     <div class="background">
@@ -12,7 +12,39 @@
 </div>
 
 <style>
-    .convorsation_container {
-        
+    input
+    {
+
+        height: 25px;
+        width: 100% ;
+        position: relative;
+        top: calc(100% - 30px);
+        padding: 15px;
+        font-size: 18px;
+        border: 2px solid var(--light_grey);
+        border-radius: 15px;
+        transition: all 200ms;
+    }
+
+    input:focus {
+        outline: none;
+        border: 2px solid var(--primary_color);
+    }
+
+    .conversation_container {
+        display: flex;
+        justify-content: center;
+        margin-top: 40vh;
+        padding: 20px;
+    }
+
+    .background {
+        z-index: -1;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background-color: var(--darkish_light);
     }
 </style>
