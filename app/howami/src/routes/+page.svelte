bc55811f624ea8105b2fb7967b62352ce0073ab0
Michael Teschner <120539187+BlazeTechDev@users.noreply.github.com>
2025-03-22 11:49:03 -0500

    Initial commit

---
+++ app/howami/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import "../globals.css"
+</script>
+
+<div>
+    <div class="convorsation_container">
+        <input>
+    </div>
+
+    <div class="background">
+    </div>
+</div>
+
+<style>
+    .convorsation_container {
+        
+    }
+</style>
