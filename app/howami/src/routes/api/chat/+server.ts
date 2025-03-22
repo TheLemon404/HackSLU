a4c6e0bab27a012b5c9bbc477849179391a2eacc
Michael Teschner <120539187+BlazeTechDev@users.noreply.github.com>
2025-03-22 18:15:52 -0500

    ai work

--- app/howami/src/routes/api/chat/+server.ts
+++ app/howami/src/routes/api/chat/+server.ts
@@ -1,9 +1,9 @@
 import type { RequestEvent } from "@sveltejs/kit";
-import { getDepressionQuestions } from "$lib/psychiatrics/asessment";
+import { formatQuestionAsResponse, getDepressionQuestions, getInitialSentiment, getQuestionListBasedOnSentiment } from "$lib/psychiatrics/asessment";
 
 function getRandomArrayElement(array: Array<any>): any
 {
-    if (array.length === 0)
+    if (!array || array.length === 0)
     {
         return undefined;
     }
@@ -15,9 +15,34 @@ function getRandomArrayElement(array: Array<any>): any
 export async function POST(event: RequestEvent): Promise<Response>
 {
     const data = await event.request.json();
+    let sentiment = {};
+    let question_chosen = "";
+    let question_chosen_pack: Array<string> = data.chosen_questions
+    let question_index = data.question_index;
+    let ready_for_results: boolean = false;
 
+    if(data.initial_sentiment)
+    {
+        sentiment = await getInitialSentiment(data.user_text)
+        question_chosen_pack = getQuestionListBasedOnSentiment(sentiment.result);
+    }
+    
+    if(question_chosen_pack.length == 0)
+    {
+        ready_for_results = true;
+    }
+
+    question_chosen = question_chosen_pack[question_index];
+    question_index++;
+
+    const question_to_ask = await formatQuestionAsResponse(question_chosen, data.user_text);
     const response = {
-        question_to_ask: getRandomArrayElement(data.depression),
+        question_to_ask: question_to_ask,
+        sentiment: sentiment,
+        initial_sentiment: false,
+        chosen_questions: question_chosen_pack,
+        chosen_question_index: question_index,
+        ready_for_results: ready_for_results,
     }
     
     return new Response(JSON.stringify(response), {
